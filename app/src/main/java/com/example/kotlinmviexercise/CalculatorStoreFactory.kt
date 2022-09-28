d229aabf1d6ed261b6d3e24096e8fc3a183cacbb
Michael Roebuck <michaelr@tradesy.com>
2022-09-28 17:21:11 -0500

    Misc Improvements

--- app/src/main/java/com/example/kotlinmviexercise/CalculatorStoreFactory.kt
+++ app/src/main/java/com/example/kotlinmviexercise/CalculatorStoreFactory.kt
@@ -1,29 +1,28 @@
 package com.example.kotlinmviexercise
 
-import com.arkivanov.mvikotlin.core.store.*
-import com.arkivanov.mvikotlin.extensions.coroutines.SuspendBootstrapper
-import com.arkivanov.mvikotlin.extensions.coroutines.SuspendExecutor
+import com.arkivanov.mvikotlin.core.store.Store
+import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.core.store.create
 import com.arkivanov.mvikotlin.keepers.statekeeper.ExperimentalStateKeeperApi
 import com.arkivanov.mvikotlin.keepers.statekeeper.StateKeeper
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
 
 @ExperimentalStateKeeperApi
 internal class CalculatorStoreFactory(private val storeFactory: StoreFactory) {
 
     fun create(stateKeeper: StateKeeper<CalculatorStore.State>?): CalculatorStore =
         // Make sure the values of the generics are consistent with the store.
-        object : CalculatorStore, Store<CalculatorStore.Intent, CalculatorStore.State, CalculatorStore.Label> by storeFactory.create(
-            name = "CounterStore",
-            initialState = CalculatorStore.State(),
-            reducer = { intent ->
-                when (intent) {
-                is CalculatorStore.Intent.Increment -> CalculatorStore.State(intent.value + 1)
-                is CalculatorStore.Intent.Decrement -> CalculatorStore.State(intent.value - 1)
-                is CalculatorStore.Intent.Sum -> sum(1000000)
-            } }
-        ){
+        object : CalculatorStore,
+            Store<CalculatorStore.Intent, CalculatorStore.State, CalculatorStore.Label> by storeFactory.create(
+                name = "CounterStore",
+                initialState = CalculatorStore.State(),
+                reducer = { intent ->
+                    when (intent) {
+                        is CalculatorStore.Intent.Increment -> CalculatorStore.State(intent.value + 1)
+                        is CalculatorStore.Intent.Decrement -> CalculatorStore.State(intent.value - 1)
+                        is CalculatorStore.Intent.Sum -> sum(1000000)
+                    }
+                }
+            ) {
         }.also {
             stateKeeper?.register {
                 // We can reset any transient state here
@@ -31,7 +30,7 @@ internal class CalculatorStoreFactory(private val storeFactory: StoreFactory) {
             }
         }
 
-    private fun sum(n: Int) : CalculatorStore.State {
+    private fun sum(n: Int): CalculatorStore.State {
         val sum = (1L..n.toLong()).sum()
         return CalculatorStore.State(sum)
     }
